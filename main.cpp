f2efb8325a8b9af9429a6a96e8794c4f6aac5ee9
Mahmoud Ramzy <mahmoudramzy1520@gmail.com>
2025-03-13 01:29:24 +0200

    Adding the search binary func and prparing the CMakeLists

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Search
+{
+public:
+    int searchInsert(vector<int> &nums, int target);
+};
+
+int Search::searchInsert(vector<int> &nums, int target)
+{
+    int l = nums.size(), f = 0, m; // last , first , middle elements
+    int index;
+    if (target > nums[l - 1])
+        return l;
+    else if (target == nums[l - 1])
+        return l - 1;
+    else if (target <= nums[f])
+        return f;
+    do
+    {
+        m = (l - f) / 2;
+        if (target == nums[m + f])
+            return index = m + f;
+        else if (target < nums[m + f])
+            l -= m;
+        else
+            f += m;
+        index = m + f;
+    } while (m > 1);
+    cout << "not existed";
+    return index;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
+        //          and Tutorial_VERSION_MINOR
+        std::cout << "Usage: " << argv[0] << " file" << std::endl;
+        return 1;
+    }
+    double const inputValue = std::stod(argv[1]);
+    return 0;
+}
